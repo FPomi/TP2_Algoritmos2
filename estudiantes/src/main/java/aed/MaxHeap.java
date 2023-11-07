70fa768dfc7080a672efb7f0dd85e979c42b7e4e
arielbakal <arielbakal.dev@gmail.com>
2023-11-07 14:02:31 -0300

    Clase MaxHeap inicializada

---
+++ estudiantes/src/main/java/aed/MaxHeap.java
@@ -0,0 +1,19 @@
+package aed;
+
+public class MaxHeap {
+
+    // variables
+
+
+    public MaxHeap(int[] s) { // Array2Heap
+
+    }
+
+    public MaxHeap() { // constructor
+
+    }
+
+    
+
+
+}
